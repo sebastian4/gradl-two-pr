7c5b28ed0ff99d2097e9f7c998fd278e6900a99a
sebastian <svm4apps@gmail.com>
2014-10-03 11:16:49 -0400

    added new spring job

--- src/main/java/slpz/grtwo/Maine.java
+++ src/main/java/slpz/grtwo/Maine.java
@@ -1,6 +1,7 @@
 package slpz.grtwo;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -15,7 +16,8 @@ public class Maine {
     public static void main(String[] args) {
         System.out.println("hello world gradl-two-pr");
         
-        greetExample();
+        //greetExample();
+        mkyong21Example();
     }
     
     public static void greetExample() {
@@ -36,6 +38,29 @@ public class Maine {
 			System.out.println("there was an error in gradl-two-pr");
 			e.printStackTrace();
 		}
+    }
+    
+    public static void mkyong21Example() {
+        System.out.println("gradl-two-pr mkyong21Example");
         
+        String[] springConfig = { "job-read-files.xml" };
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("readMultiFileJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+			System.out.println("Exit Status : " + execution.getAllFailureExceptions());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		System.out.println("Done");
     }
 }
